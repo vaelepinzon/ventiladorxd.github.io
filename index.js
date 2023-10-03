b711585575ac3a4c4a7624ca5e5f166b7d281833
vaelepinzon <146895340+vaelepinzon@users.noreply.github.com>
2023-10-03 18:18:21 -0500

    ventilador

---
+++ index.js
@@ -0,0 +1,10 @@
+var iconMenu = document.querySelector("#iconMenu");
+var memu = document.querySelector("#menu");
+
+iconMenu.addEventListener('click', () => {
+      if (menu.style.display == "none") {
+            menu.style.display = "flex";
+      } else {
+            menu.style.display = "none";
+      }
+})
